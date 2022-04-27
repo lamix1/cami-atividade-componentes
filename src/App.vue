95e13bb3495b9b46e55eb9823a5d3556be80762c
lamix1 <camilacristina328santoss@gmail.com>
2022-04-26 21:19:24 -0300

    Adicionando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script>
 import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhoPrincipal from "./components/CabecalhoPrin.vue";
+import MenuPrincipal from "./components/MenuPrin.vue";
+import RodapePrincipal from "./components/RodapePrin.vue";
 export default {
-  components: { MenuLateral },
+  components: {
+    MenuLateral,
+    CabecalhoPrincipal,
+    MenuPrincipal,
+    RodapePrincipal,
+  },
 };
 </script>
 <template>
   <MenuLateral />
-  <header id="header">Cabeçalho</header>
-  <main id="content">Principal</main>
-  <footer id="footer">Rodapé</footer>
+  <CabecalhoPrincipal />
+  <MenuPrincipal />
+  <RodapePrincipal />
 </template>
 <style></style>
